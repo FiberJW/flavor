09ea79796066046d3fc9ece3063b0a6d73916037
Juwan Wheatley <datwheat@gmail.com>
2016-04-05 08:09:51 -0400

    added recipes

---
+++ recipes/rubyish.flavor.config.js
@@ -0,0 +1,62 @@
+/*
+ * This allows Ruby-esque code to be written and compiled to JS
+ * 
+ * unless (true) do
+ *   return;
+ * end
+ * elif (true) do
+ *   print(true or false == true)
+ * end
+ *
+ * will compile to 
+ *
+ * if (!true) {
+ *   return;
+ * }
+ * else if (true) {
+ *   console.log(true || false === true)
+ * }
+ */
+
+module.exports.keys = [
+  {
+    alias: /\b(print\()\b/g,
+    translation: 'console.log('
+  },
+  {
+    alias: /\b(elif\s?\()/g,
+    translation: 'else if ('
+  },
+  {
+    alias: /\b(or)\b/g,
+    translation: '||'
+  },
+  {
+    alias: /\s(\=\=)\s/g,
+    translation: ' === '
+  },
+  {
+    alias: /\b(and)\b/g,
+    translation: '&&'
+  },
+  {
+    alias: /\b(unless\s?\()/g,
+    translation: 'if (!'
+  },
+  {
+    alias: /\b(is)\b/g,
+    translation: '==='
+  },
+  {
+    alias: /\b(do)\b/g,
+    translation: '{'
+  },
+  {
+    alias: /\b(end)\b/g,
+    translation: '}'
+  },
+  {
+    alias: /\b(next)\b/g,
+    translation: 'continue'
+  }
+]
